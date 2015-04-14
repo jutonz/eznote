c33e33ef828d71876107fab8ab2fa9548e5d30aa
Justin Toniazzo <jutonz42@gmail.com>
2015-04-14 13:33:21 -0400

    Initial commit

---
+++ app/views/notes/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @note, :id, :text, :favorite?, :created_at, :updated_at
---
+++ db/migrate/20150414173123_create_notes.rb
@@ -0,0 +1,10 @@
+class CreateNotes < ActiveRecord::Migration
+  def change
+    create_table :notes do |t|
+      t.string :text
+      t.boolean :favorite?
+
+      t.timestamps null: false
+    end
+  end
+end
