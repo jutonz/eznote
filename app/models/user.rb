1ab12ee49a209274739ab2c5cae7284e428a7554
Justin Toniazzo <jutonz42@gmail.com>
2015-04-16 18:22:24 -0400

    User credential validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,20 @@
 class User < ActiveRecord::Base
+  has_many :notes
+  has_secure_password
+
+  before_save do self.email.downcase! end
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, uniqueness: { case_sensitive: false },
+                    format:     { with: VALID_EMAIL_REGEX },
+                    presencce:  true
+  validates :password, lemgth: { minimum: 6 }
+  validates :display_name, presence: true, length: { maximum: 50 }
+
+  def logged_in?
+    persisted?
+  end
+
+  def display_name
+    read_attribute(:display_name) || 'Guest'
+  end
 end
