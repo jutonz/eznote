ee5acddd8dcd926cbeec56feb0de8accff91f167
Justin Toniazzo <jutonz42@gmail.com>
2015-04-16 18:27:08 -0400

    Sessions

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,28 @@
+class SessionsController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.find_by email: user_params[:email]
+    if @user && @user.authenticate(user_params[:password])
+      session[:user_id] = @user.id
+      redirect_to root_path
+    else
+      redirect_to login_path, flash: { error: 'Incorrect username or password' }
+    end
+  end
+
+  def destroy
+    if current_user
+      session.delete :user_id
+    end
+    redirect_to root_path
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Rails.application.routes.draw do
   resources :notes
+  resources :users
+
+  get    '/signup',   to: 'users#new'
+  post   '/signup',   to: 'users#create'
+  get    '/login',    to: 'sessions#new'
+  post   '/login',    to: 'sessions#create'
+  delete '/logout',   to: 'sessions#destroy'
 
   root 'notes#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
