d5214fa987a47610e6af112019a33a0aaeb6d937
Justin Toniazzo <jutonz42@gmail.com>
2015-04-22 00:02:08 -0400

    Rework all notes page

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,5 +1,6 @@
 class NotesController < ApplicationController
   before_action :set_note, only: [:show, :edit, :update, :destroy]
+  before_action :set_user
 
   # GET /notes
   # GET /notes.json
@@ -68,6 +69,10 @@ class NotesController < ApplicationController
     # Use callbacks to share common setup or constraints between actions.
     def set_note
       @note = Note.find(params[:id])
+    end
+
+    def set_user
+      @current_user = current_user
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
